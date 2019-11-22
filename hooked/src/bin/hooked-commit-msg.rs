7f3d02a01bc5174cd9e2a4b1c44be64534cb1a37
Michael Gattozzi <mgattozzi@gmail.com>
2019-11-22 15:52:31 -0500

    Add commit message linting hook to the repo
    This adds a commit to handle git commit linting to enforce style by not
    allowing less than 10 or more than 50 chars for titles and less than or
    equal to 72 chars for the body. Chars are measured in number of
    graphemes as 50 chars represented in the terminal is what we want to use
    not 50 bytes. This will eventually be an installable hook for end users
    if they want it.

--- hooked/src/bin/hooked-commit-msg.rs
+++ hooked/src/bin/hooked-commit-msg.rs
@@ -1,3 +1,47 @@
+use shared::find_root;
+use std::{
+  env::args,
+  error::Error,
+  fs,
+  process,
+};
+use unicode_segmentation::UnicodeSegmentation;
+
 fn main() {
-  println!("Hello, world!");
+  if let Err(e) = || -> Result<(), Box<dyn Error>> {
+    let path = find_root()?.join(args().last().ok_or_else(|| {
+      "Expected to be passed a path to the git commit message"
+    })?);
+
+    let file = fs::read_to_string(path)?;
+    let mut lines = file.lines();
+
+    if let Some(ref first_line) = lines.next() {
+      let length = first_line.graphemes(true).count();
+      if length > 50 {
+        return Err(
+          "Your commit header is over 50 characters (i.e. graphemes) in length.\n\
+           Commit messages titles should be between 10 to 50 characters".into());
+      }
+      if length < 10 {
+        return Err(
+          "Your commit header is less than 10 characters (i.e. graphemes) in length.\n\
+           Commit messages titles should be between 10 to 50 characters".into());
+      }
+    }
+
+    for line in lines {
+      let length = line.graphemes(true).count();
+      if length > 72 {
+        return Err(
+          "One of the lines in the body of the commit is over 72 characters (i.e. graphemes) in \n\
+           length. Commit messages titles should be between 10 to 50 characters".into());
+      }
+    }
+
+    Ok(())
+  }() {
+    eprintln!("{}", e);
+    process::exit(1);
+  }
 }
