ed50b2c64ac1509ec704144c576fb72939982c78
Michael Gattozzi <mgattozzi@gmail.com>
2020-01-01 19:10:48 -0500

    Make hooks cross platform

--- .dev-suite/hooked/pre-commit.py
+++ .dev-suite/hooked/pre-commit.py
@@ -2,6 +2,6 @@
 import subprocess
 
 subprocess.run("cargo build --all", shell=True, check=True)
-subprocess.run("cargo test --all", shell=True, check=True)
+subprocess.run("cargo test --all -- --test-threads=1", shell=True, check=True)
 subprocess.run("rustup run nightly cargo fmt --all -- --check", shell=True, check=True)
 subprocess.run("cargo clippy --all --all-targets -- -W clippy::pedantic", shell=True, check=True)
